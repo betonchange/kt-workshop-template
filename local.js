6034be729c7158ffde560d2aca4d4cfbd22e3537
betonchange <51466485+betonchange@users.noreply.github.com>
2019-06-06 21:07:36 -0400

    Update local.js

--- local.js
+++ local.js
@@ -56,9 +56,7 @@ var duration = moment.duration(between_now_and_event).minutes();
 const marquee_message =
   `<li id="xkcd"><marquee>
 
-${days_to_event} Days till Trump's regime ends IF YOU VOTE. IF NOT, ${days_to_event + 1460} Days. Go Vote.
-
-1.5˚ Climate Crisis ${years_to_event} years <span id="minutes" class='part'>${duration}</span>min
+${days_to_event} Days till Trump's regime ends IF YOU VOTE. IF NOT, (${days_to_event}+1460) Days. Go Vote.
 
   </marquee></li>`;
 
